db882c2fca20baf3b98ce5dcbf34ebacb7606863
muiscript <myam0118@gmail.com>
2019-03-09 23:29:18 +0900

    implement basic logic to add activity

--- cli/cli.go
+++ cli/cli.go
@@ -1,15 +1,59 @@
 package cli
 
-const (
-	Success = iota
+import (
+	"errors"
+	"gh-activist/gh"
+	"log"
+	"os"
+	"time"
 )
 
-type Cli struct {}
+const (
+	Success = iota
+	CommitError
+)
+
+type Cli struct {
+	ghClient *gh.Client
+	logger   *log.Logger
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
 
 func New() (*Cli, error) {
-	return &Cli{}, nil
+	user := getEnv("GH_USER", "muiscript")
+	token := getEnv("GH_TOKEN", "")
+	if token == "" {
+		return nil, errors.New("GH_TOKEN variable empty")
+	}
+	repo := getEnv("GH_REPO", "gh-activist")
+	file := getEnv("GH_REPO", "commit.txt")
+	ghClient := gh.NewClient(user, token, repo, file)
+
+	logFile := getEnv("LOG_FILE", "")
+	logWriter, err := os.Open(logFile)
+	if err != nil {
+		logWriter = os.Stdout
+	}
+	logger := log.New(logWriter, "[gh-activist]", log.LstdFlags)
+
+	return &Cli{
+		ghClient: ghClient,
+		logger:   logger,
+	}, nil
 }
 
 func (c *Cli) Run() int {
+	now := time.Now().String()
+	if err := c.ghClient.Commit(now, now); err != nil {
+		return CommitError
+	}
+
 	return Success
 }
